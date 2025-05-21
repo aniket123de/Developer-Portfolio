24abb15c9580fb84855783469bf0fea63e70bcb8
aniket123de <aniketde1234@gmail.com>
2025-05-21 19:29:32 +0530

    Fixing

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -6,10 +6,11 @@ import { staggerContainer } from "../utils/motion";
 const StarWrapper = (Component, idName) =>
   function HOC() {
     return (
-      <motion.section        variants={staggerContainer()}
+      <motion.section        
+        variants={staggerContainer()}
         initial='hidden'
         whileInView='show'
-        viewport={{ once: false, amount: 0.25 }}
+        viewport={{ once: true, amount: 0.25 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
